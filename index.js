8bc8b7788adec99965c679a7d6cfdfd041b7116b
Wandia E Mugo <wwandia173@gmail.com>
2022-06-11 16:52:24 +0300

    commit tree e111f2125ded38a72a62fefde47399e32b6a5558

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const testVar = "h1";
+document.querySelector("main#main").remove();
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Elle is the champion";
+//newHeader.className = "victory";
+const victory = document.getElementById("victory");
+victory.innerHTML=
+"<h1>YOUR-NAME is the champion</h1>"
