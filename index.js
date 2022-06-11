dde714884c3fe6d89c190f1fa63a4f888739dff5
Wandia E Mugo <wwandia173@gmail.com>
2022-06-11 16:53:07 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const testVar = "h1";
+document.querySelector("main#main").remove();
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Elle is the champion";
+const victory = document.getElementById("victory");
+victory.innerHTML=
+"<h1>YOUR-NAME is the champion</h1>"
